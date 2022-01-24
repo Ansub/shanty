5162fbdb57042fc4b627a50d55c7ccf13a9fc129
Ansub Khan <khanansub3@gmail.com>
2022-01-24 17:23:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const author = document.getElementById("author")
+const crypto = document.querySelector(".crypto-top")
+const quote = document.querySelector(".quote")
+
+// Unsplash API
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById("author").textContent = `By ${data.user.name}`
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+    document.getElementById("author").textContent = `By Dodi Achmad`
+  })
+
+//    Crypto Prices
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Csolana%2Cdogecoin&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    crypto.innerHTML = `
+    <span> <img class="crypto-img"src="Images/bitcoin.png"/>$${data.bitcoin.usd}</span>
+    <span> <img class="crypto-img"src="Images/ethereum.png"/>$${data.ethereum.usd}</span>
+`
+  })
+
+//     Timer
+function currentTime() {
+  time = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  })
+  document.querySelector(".time").innerHTML = time.toUpperCase()
+}
+
+setInterval(currentTime, 1000)
+// Motivation Quote
+
+fetch("https://api.quotable.io/random?tags=inspirational")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+    quote.innerHTML = `
+      <p>${data.content}</p>
+      <p>${data.author}</p>
+      `
+  })
+  .catch((err) => console.log(err))
